b665ba26dc4bc546c1d566bdae2be016fbc80123
Mayank Anand <mayankan@gmail.com>
2022-04-01 15:52:24 +0530

    [Mayank] feat: Adding implemented Address Book Main Program

---
+++ address_book_main.py
@@ -0,0 +1,15 @@
+"""
+    @Author: Mayank Anand
+    @Date: 2022=04-01
+    @Last Modified by: Mayank Anand
+    @Last Modified time: 2022=04-01
+    @Title : Displaying Welcome to Address Book Program
+"""
+
+
+def main():
+    print("Welcome to Address Book Program")
+
+
+if __name__ == "__main__":
+    main()
