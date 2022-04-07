8315e7f067d88b6d14772cce155517baacfcfa82
Mayank Anand <mayankan@gmail.com>
2022-04-07 23:09:16 +0530

    [Mayank] test: Adding working test cases for sort by zip code

--- test_sorted_zip.py
+++ test_sorted_zip.py
@@ -15,16 +15,20 @@ class TestSortedContactsZip(ut.TestCase):
     def test_sorted_zip(self):
         # Checking correct input.
         self.assertEqual(contact_instance.view_sorted_contacts_zip(), 
-        [{'f_name': 'Mayank', 'l_name': 'Anand','address': 'B8, Acharya Niketan', 'city': 'Mayur Vihar Phase 1', 
-        'state': 'Delhi', 'zip': 110091, 'phone_no': 9560291169, 'email': 'mayankan@gmail.com'},
-        {'f_name': '11', 'l_name': 'Anand','address': 'B8, Acharya Niketan', 'city': 'Mayur Vihar', 
-        'state': 'Delhi', 'zip': 110091, 'phone_no': 9560291169, 'email': 'mayankan@gmail.com'}])
+        [{'f_name': 'Mayank', 'l_name': 'Gupta','address': 'B8, Acharya Niketan', 'city': 'Okhla', 
+        'state': 'Delhi', 'zip_code': 110091, 'phone_no': 9560291169, 'email': 'mayankan@gmail.com'},
+        {'f_name': 'Mayank', 'l_name': 'Anand','address': 'B8, Acharya Niketan', 'city': 'Mayur Vihar', 
+        'state': 'Delhi', 'zip_code': 210091, 'phone_no': 9560291169, 'email': 'mayankan@gmail.com'},
+        {'f_name': 'Mayank', 'l_name': 'Bhalla','address': 'B8, Acharya Niketan', 'city': 'Mayur Vihar Phase 1', 
+        'state': 'Uttar Pradesh', 'zip_code': 910091, 'phone_no': 9560291169, 'email': 'mayankan@gmail.com'}])
         # Checking of List is not sorted
         self.assertNotEqual(contact_instance.view_sorted_contacts_zip(), 
-        [{'f_name': '11', 'l_name': 'Anand','address': 'B8, Acharya Niketan', 'city': 'Mayur Vihar', 
-        'state': 'Delhi', 'zip': 110091, 'phone_no': 9560291169, 'email': 'mayankan@gmail.com'},
-        {'f_name': 'Mayank', 'l_name': 'Anand','address': 'B8, Acharya Niketan', 'city': 'Mayur Vihar Phase 1', 
-        'state': 'Delhi', 'zip': 110091, 'phone_no': 9560291169, 'email': 'mayankan@gmail.com'}])
+        [{'f_name': 'Mayank', 'l_name': 'Anand','address': 'B8, Acharya Niketan', 'city': 'Mayur Vihar', 
+        'state': 'Delhi', 'zip_code': 210091, 'phone_no': 9560291169, 'email': 'mayankan@gmail.com'},
+        {'f_name': 'Mayank', 'l_name': 'Bhalla','address': 'B8, Acharya Niketan', 'city': 'Mayur Vihar Phase 1', 
+        'state': 'Uttar Pradesh', 'zip_code': 910091, 'phone_no': 9560291169, 'email': 'mayankan@gmail.com'},
+        {'f_name': 'Mayank', 'l_name': 'Gupta','address': 'B8, Acharya Niketan', 'city': 'Okhla', 
+        'state': 'Delhi', 'zip_code': 110091, 'phone_no': 9560291169, 'email': 'mayankan@gmail.com'}])
 
 if __name__ == "__main__":
     ut.main()
