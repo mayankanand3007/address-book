433c853f16abab01775eb1e317edd8ab7b43c8c1
Mayank Anand <mayankan@gmail.com>
2022-04-07 08:56:45 +0530

    [Mayank] test: Adding test for Sort Contacts by Zip code

---
+++ test_sorted_zip.py
@@ -0,0 +1,30 @@
+"""
+    @Author: Mayank Anand
+    @Date: 2022-04-06
+    @Last Modified by: Mayank Anand
+    @Last Modified time: 2022-04-07
+    @Title : Testing ability to Sort Contacts by Zip Code in Address Book Program
+"""
+import unittest as ut
+from contacts import Contact
+
+
+contact_instance = Contact()
+
+class TestSortedContactsZip(ut.TestCase):
+    def test_sorted_zip(self):
+        # Checking correct input.
+        self.assertEqual(contact_instance.view_sorted_contacts_zip(), 
+        [{'f_name': 'Mayank', 'l_name': 'Anand','address': 'B8, Acharya Niketan', 'city': 'Mayur Vihar Phase 1', 
+        'state': 'Delhi', 'zip': 110091, 'phone_no': 9560291169, 'email': 'mayankan@gmail.com'},
+        {'f_name': '11', 'l_name': 'Anand','address': 'B8, Acharya Niketan', 'city': 'Mayur Vihar', 
+        'state': 'Delhi', 'zip': 110091, 'phone_no': 9560291169, 'email': 'mayankan@gmail.com'}])
+        # Checking of List is not sorted
+        self.assertNotEqual(contact_instance.view_sorted_contacts_zip(), 
+        [{'f_name': '11', 'l_name': 'Anand','address': 'B8, Acharya Niketan', 'city': 'Mayur Vihar', 
+        'state': 'Delhi', 'zip': 110091, 'phone_no': 9560291169, 'email': 'mayankan@gmail.com'},
+        {'f_name': 'Mayank', 'l_name': 'Anand','address': 'B8, Acharya Niketan', 'city': 'Mayur Vihar Phase 1', 
+        'state': 'Delhi', 'zip': 110091, 'phone_no': 9560291169, 'email': 'mayankan@gmail.com'}])
+
+if __name__ == "__main__":
+    ut.main()
