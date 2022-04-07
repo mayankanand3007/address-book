4a2cfcba13a3a58917ae6f8e51a80a5552afdb66
Mayank Anand <mayankan@gmail.com>
2022-04-07 13:44:31 +0530

    [Mayank] fix: Updating and checking correct values of add contact in main

--- address_book_main.py
+++ address_book_main.py
@@ -3,7 +3,7 @@
     @Date: 2022-04-01
     @Last Modified by: Mayank Anand
     @Last Modified time: 2022-04-05
-    @Title : Displaying Welcome to Address Book Program
+    @Title : Displaying Welcome and Adding Contact to Address Book Program
 """
 from contacts import Contact
 
@@ -22,12 +22,13 @@ def add_contact_inputs():
     f_name = input("Enter your first name: ")
     l_name = input("Enter your last name: ")
     address = input("Enter your address: ")
+    city = input("Enter your city: ")
     state = input("Enter your state: ")
     zip_code = int(input("Enter your zip code: "))
     phone_no = int(input("Enter your phone number: "))
     email = input("Enter your email address: ")
-    contact_instance.add_contact(f_name, l_name, address, state, zip_code, phone_no, email)
-    return f_name
+    contact_instance.add_contact(f_name, l_name, address, city, state, zip_code, phone_no, email)
+    return f"{f_name} Contact added."
 
 def view_contacts():
     """
@@ -44,7 +45,7 @@ def view_contacts():
     for contact in contacts:
         contact_list += f"Contact {count}\nFirst Name: {contact['f_name']}, " \
             f"Last Name: {contact['l_name']}, Address: {contact['address']}, State: {contact['state']}, " \
-                f"Zip Code: {contact['zip']}, Phone No. {contact['phone_no']} and Email: {contact['email']}\n"
+                f"Zip Code: {contact['zip_code']}, \nPhone No. {contact['phone_no']} and Email: {contact['email']}\n"
         count += 1
     return contact_list
 
@@ -52,12 +53,21 @@ def view_contacts():
 def main():
     while True:
         print("Welcome to Address Book Program")
-        print_stmts = ["Add Contact"]
+        print_stmts = ["Add Contact", "Display all contacts"]
         for print_stmt in range(len(print_stmts)):
             print(f"{print_stmt+1} - {print_stmts[print_stmt]}")
         # Asks user for input from the above options.
-        operation_number = int(input("Enter the above number(1) to do the following operation: "))
-        print(f"{add_contact_inputs()} Contact adeed")
+        operation_number = int(input("Enter the above number(1-2) to do the following operation: "))
+        switcher = {
+            1: add_contact_inputs,
+            2: view_contacts,
+        }
+        # Checks if input given by the user is between 1 and 4 else asks the input again.
+        if 0 < operation_number <= 2:
+            print(switcher.get(operation_number)())
+        else:
+            print("Invalid number entered. Please try again: ")
+            continue
         # Checks if user wants to end the loop of performing operations in Phone Book.
         if input('Do you want to check your Address Book again?(y/n): ') != 'y':
             break
