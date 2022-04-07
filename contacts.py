11ffd2e71085259909971bac9dfe86f32ca95adf
Mayank Anand <mayankan@gmail.com>
2022-04-07 14:46:43 +0530

    [Mayank] refactor: Updating view contact in delete contact logic

--- contacts.py
+++ contacts.py
@@ -2,15 +2,15 @@
     @Author: Mayank Anand
     @Date: 2022-04-01
     @Last Modified by: Mayank Anand
-    @Last Modified time: 2022-04-06
+    @Last Modified time: 2022-04-07
     @Title : Ability to add, edit and delete Contact in Address Book Program
 """
 class Contact:
     def __init__(self):
         self.contacts = [{'f_name': 'Mayank', 'l_name': 'Anand','address': 'B8, Acharya Niketan', 'city': 'Mayur Vihar Phase 1', 
-        'state': 'Delhi', 'zip': 110091, 'phone_no': 9560291169, 'email': 'mayankan@gmail.com'},
-        {'f_name': 11, 'l_name': 'Anand','address': 'B8, Acharya Niketan', 'city': 'Mayur Vihar Phase 1', 
-        'state': 'Delhi', 'zip': 110091, 'phone_no': 9560291169, 'email': 'mayankan@gmail.com'}]
+        'state': 'Delhi', 'zip_code': 110091, 'phone_no': 9560291169, 'email': 'mayankan@gmail.com'},
+        {'f_name': 'Mayank', 'l_name': 'Anand', 'address': '301, Pocket 5, Mayur Vihar Phase 1', 'city': 'New Delhi', 
+        'state': 'Delhi', 'zip_code': 110091, 'phone_no': 9560291169, 'email': 'mayankan@gmail.com'}]
     
     def add_contact(self, f_name, l_name, address, city, state, zip_code, phone_no, email):
         """
@@ -26,17 +26,16 @@ class Contact:
             phone_no: Phone Number of the Person to be added as Contact.
             email: Email Address of the Person to be added as Contact.
         Return:
-            Dictionary of the Contact added with key value pair of the Person details.
+            Dictionary of the All Contacts after adding Person's Contact.
         """
         if type(phone_no) != int:
             raise TypeError("The Phone Number can only be a number.")
         if type(zip_code) != int:
             raise TypeError("The Zip Code can only be a number.")
         self.contacts.append({'f_name': f_name, 'l_name': l_name, 
-        'city': city, 'state': state, 'address': address, 'zip': zip_code, 'phone_no': phone_no, 'email': email})
-        return {'f_name': f_name, 'l_name': l_name, 
-        'city': city, 'state': state, 'address': address, 'zip': zip_code, 'phone_no': phone_no, 'email': email}
-    
+        'city': city, 'state': state, 'address': address, 'zip_code': zip_code, 'phone_no': phone_no, 'email': email})
+        return self.contacts
+
     def edit_contact_fname(self, f_name, l_name, address, city, state, zip_code, phone_no, email):
         """
         Description:
@@ -51,7 +50,7 @@ class Contact:
             phone_no: Phone Number of the Person to be added as Contact.
             email: Email Address of the Person to be added as Contact.
         Return:
-            Dictionary of the Contact edited with key value pair of the Person details.
+            Dictionary of the All Contacts after editing Person's Contact.
         """
         contacts = self.contacts
         if type(phone_no) != int:
@@ -67,8 +66,8 @@ class Contact:
                 contacts[contact]["zip_code"] = zip_code
                 contacts[contact]["phone_no"] = phone_no
                 contacts[contact]["email"] = email
-        return {'f_name': f_name, 'l_name': l_name, 
-        'city': city, 'state': state, 'address': address, 'zip': zip_code, 'phone_no': phone_no, 'email': email}
+        return {'f_name': f_name, 'l_name': l_name,'address': address, 'city': city, 
+        'state': state, 'zip_code': zip_code, 'phone_no': phone_no, 'email': email}
 
     def delete_contact_fname(self, f_name):
         """
@@ -77,10 +76,24 @@ class Contact:
         Parameter:
             f_name: First name of the Person to be used to fetch Contact.
         Return:
-            First Name value of Contact which is deleted from Address Book.
+            Dictionary of the All Contacts after deleting Person's Contact.
         """
         contacts = self.contacts
+        if type(f_name) != str:
+            raise TypeError("The Phone Number can only be a string.")
         for contact in contacts:
             if contact["f_name"] == f_name:
                 contacts.remove(contact)
+        return contacts
+
+    def view_contacts(self):
+        """
+        Description:
+            Returns Contacts available in the current Address Book.
+        Parameter:
+            None.
+        Return:
+            Contacts avaiable in the current Address Book.
+        """
+        contacts = self.contacts
         return contacts
